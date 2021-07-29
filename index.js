850b14f364a02b8a32ef1821b527ac19a29f2a6d
troymeeker <troymeeker1@gmail.com>
2021-07-29 12:50:43 -0700

    complete

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+return fetch('https://anapioficeandfire.com/api/books')
+.then(resp=>resp.json())
+.then(data => renderBooks(data))
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -11,6 +13,7 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
+renderBooks();
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
